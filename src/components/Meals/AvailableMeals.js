8b1e41cd47f29c16b0e93312453d7e744aa4922a
AyjayJr <ayjayjr@hotmail.com>
2023-03-19 15:02:26 -0400

    styled meal list, added new meal item, meal item form, and input components

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,6 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -28,15 +31,24 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+  const mealsList = DUMMY_MEALS.map(meal => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
-	return (
-		<section>
-			<ul className={classes.meals}>
-				{mealsList}
-			</ul>
-		</section>
-	);
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>
+          {mealsList}
+        </ul>
+      </Card>
+    </section>
+  );
 };
 
 export default AvailableMeals;
